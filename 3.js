128461ede4048ef5afad1395039e920b344156fb
yangtaeho <yangbuk84@gmail.com>
2019-02-17 22:54:37 +0900

    3.3 구현... 결과 제대로 안 나옴..

--- 3.js
+++ 3.js
@@ -17,31 +17,40 @@ const std = {
   }
 };
 
-const mark_sieve = (first, last, factor, res = [])=>{
+/**
+ * 
+ * @param {*} first factor 의 배수 중에서 아직 지워지지 않은 첫 번째 배수에 해당하는 불 값을 가리킴 
+ * @param {*} last stl 관행에 따라 우리가 사용할 표에 들어있는 마지막 원소 바로 다음 위치를 가리키는 반복자
+ * @param {*} factor  지금 처리할 소수 인수
+ * @param {*} res 책과 달리 c++ 같이 메모리 참조를 하지 않고 처리된 값이 저장된 배열을 넘겨줌
+ * **** last - first 가 원소의 갯수
+ */
+const mark_sieve = (first, last, factor, flags = [])=>{
   // assert(first != last)
+  let i = 0;
   //*first = false;
-  res[first] = false;
+  flags[i++] = false;
   while (last - first > factor) {
     first = first + factor;
     //*first = false;
-    res[first] = false;
+    flags[i++] = false;
   }
 };
-const getSieves = (nums, flags)=>{
+
+// 테스트용 함수
+const getSieves = (flags)=>{
   let i = 0;
   return flags.reduce((r, v)=>{
-    v && r.push(nums[i]);
     i++;
+    v && r.push(i);
     return r;
   }, []).join(', ');
 };
 (()=>{ //test
-  const nums = std.generate(1, 13, v=>++v);
-  let flags = std.fill(nums[0], nums[nums.length - 1], true);
-  mark_sieve(nums[0], nums[nums.length - 1], 2, flags);
-  mark_sieve(nums[0], nums[nums.length - 1], 3, flags);
-  console.log(getSieves(nums, flags));
-  console.log('\n', nums, '\n', flags);
+  let flags = std.fill(0, 43, true);
+  mark_sieve(4, 43, 2, flags);
+  mark_sieve(6, 43, 3, flags);
+  console.log('테스트', '\n', flags, '\n', getSieves(flags));
 })();
 
 
@@ -67,22 +76,29 @@ const my_ratio = (x, y)=>{
 
 
 
-const sift0 = (_first, n, res = [])=>{
-  const flags = std.fill(_first, _first + n, true);
+const sift0 = (_first, n, nums)=>{
+  let flags = std.fill(_first, _first + n, true);
   //std.fill(_first, _first + n, true);
   let i = 0;
   let index_square = 3;
   while (index_square < n) {
     // index_square = 2i^2 + 6i + 3으로 고정
     if (flags[i]) {
-      mark_sieve(res[i] + index_square,
-          res[i] + n,
-          i + i + 3, res);
+      mark_sieve(_first + index_square,
+          _first + n,
+          i + i + 3, flags);
     }
     ++i;
     index_square = 2*i*(i + 3) + 3;
+    console.log('debug', i, _first, n, index_square, nums[i], flags[i]);
   }
+  return flags; // 추가함...
 };
+(()=>{
+  const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98];
+  let flags = sift0(0, 98, nums);
+  console.log('테스트 2', '\n', flags, '\n', getSieves(flags));
+})();
 
 
 const sift1 = (_first, n)=>{
@@ -123,3 +139,18 @@ const sift = (first, n)=>{
     index_square += factor;
   }
 }
+
+
+// 3.4
+// class line_segment {
+
+// }
+// line_segment gcm = (line_segment a, line_segment b) => {}
+// const gcm = (a, b) => {
+//   if (a==b) return a;
+//   if (b < a) return gcm(a - b, b);
+//   /* if (a < b)  */ return gcm(a, b - a);
+// };
+// line segment 가 끝없이 들어가게 된다.
+
+
